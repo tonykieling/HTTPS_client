c01e2ddaf3837b7ee631b9b9106c20be6c509749
Tony Kieling <tony.kieling@gmail.com>
2019-01-14 21:16:32 +0000

    step2 done

--- step2.js
+++ step2.js
@@ -1,32 +1,37 @@
-let https = require('https');
 
-// let destiny = process.argv[2];
+function getAndPrintHTML(){
+  let https = require('https');
 
-// if (!destiny) {
-//   return (console.log("Please set the destiny"));
-// }
+  // let destiny = process.argv[2];
 
-let destiny = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step1.html'
+  // if (!destiny) {
+  //   return (console.log("Please set the destiny"));
+  // }
+
+  let destiny = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  }
+
+  let wholeContent = "";
+
+  https.get(destiny, function (response){
+    response.setEncoding('utf8');
+
+    let count = 0;
+    response.on('data', function (data){
+      console.log('Chunck received! ', data);
+      wholeContent += data + " --- NEWLINE (" + ++count + ")\n";
+      console.log(data + " --- NEWLINE (" + count + ")\n");
+    });
+
+    response.on('end', function (){
+      console.log('Response stream has completed.');
+      console.log("ending the process");
+      console.log("the wholeContent: \n" + wholeContent);
+    });
+  console.log("done!");
+  });
 }
 
-let wholeContent = "";
-
-https.get(destiny, function (response){
-  response.setEncoding('utf8');
-
-  let count = 0;
-  response.on('data', function (data){
-    console.log('Chunck received! ', data);
-    wholeContent += data + " --- NEWLINE (" + ++count + ")\n";
-    console.log(data + " --- NEWLINE (" + ++count + ")\n");
-  });
-
-  response.on('end', function (){
-    console.log('Response stream has completed.');
-    console.log("ending the process");
-    console.log("the wholeContent: \n" + wholeContent);
-  });
-console.log("done!");
-})
+getAndPrintHTML();
